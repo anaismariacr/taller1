f9ac45d9ace539a67c64350a8411646ca5c25c06
anaismariacr <am.castro23@uniandes.edu.co>
2025-04-06 21:11:00 -0500

    promedio temporadas added

--- Main.ts
+++ Main.ts
@@ -3,14 +3,21 @@ import { series } from "./Data.js";
 const tableBody = document.querySelector("tbody")!;
 
 tableBody.innerHTML = ""; 
+    series.forEach(serie => {
+    const row = document.createElement("tr");
+    row.innerHTML = `
+        <th scope="row">${serie.id}</th>
+        <td>${serie.name}</td>
+        <td>${serie.channel}</td>
+        <td>${serie.seasons}</td>
+    `;
+    tableBody.appendChild(row);
+    });
 
-series.forEach(serie => {
-  const row = document.createElement("tr");
-  row.innerHTML = `
-    <th scope="row">${serie.id}</th>
-    <td>${serie.name}</td>
-    <td>${serie.channel}</td>
-    <td>${serie.seasons}</td>
-  `;
-  tableBody.appendChild(row);
-});
+
+const averageContainer: HTMLElement = document.getElementById("average-seasons")!;
+const totalTemporadas = series.reduce((sum, serie) => sum + serie.seasons, 0);
+const promedio = totalTemporadas / series.length;
+    
+averageContainer.innerText = `Seasons average: ${promedio.toFixed(2)}`;
+
--- dist/main.js
+++ dist/main.js
@@ -4,10 +4,14 @@ tableBody.innerHTML = "";
 series.forEach(serie => {
     const row = document.createElement("tr");
     row.innerHTML = `
-    <th scope="row">${serie.id}</th>
-    <td>${serie.name}</td>
-    <td>${serie.channel}</td>
-    <td>${serie.seasons}</td>
-  `;
+        <th scope="row">${serie.id}</th>
+        <td>${serie.name}</td>
+        <td>${serie.channel}</td>
+        <td>${serie.seasons}</td>
+    `;
     tableBody.appendChild(row);
 });
+const averageContainer = document.getElementById("average-seasons");
+const totalTemporadas = series.reduce((sum, serie) => sum + serie.seasons, 0);
+const promedio = totalTemporadas / series.length;
+averageContainer.innerText = `Seasons average: ${promedio.toFixed(2)}`;
